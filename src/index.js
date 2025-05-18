aa004ef32659e783e43455fb0f120ffe9c2d3f90
TreanEri84 <59999853+TreanEri84@users.noreply.github.com>
2025-05-18 14:46:08 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 import { ThemeProvider } from './context/ThemeContext';
-import { DarkModeProvider } from './context/DarkModeContext';
-import { CurrencyProvider } from './context/CurrencyContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider>
-    <DarkModeProvider>
-      <CurrencyProvider>
-        <App />
-      </CurrencyProvider>
-    </DarkModeProvider>
+    <App />
   </ThemeProvider>
 );
